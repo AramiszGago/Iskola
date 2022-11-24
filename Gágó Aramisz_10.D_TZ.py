d8124f46782d454f960e5567f5f43ef1bfecf52c
AramiszGago <114133710+AramiszGago@users.noreply.github.com>
2022-11-24 09:00:29 +0100

    Add files via upload

---
+++ Gágó Aramisz_10.D_TZ.py
@@ -0,0 +1,43 @@
+szam = float(input("Adj meg egy valós számot:"))
+while szam < -1:
+    szam = float(input("Adj meg egy valós számot:"))
+    if szam == -1:
+        print("Program vége!")
+    else:
+        print("Add meg a mérték egységet (cm, m, km):")
+        mertekegyseg = input()
+        print("Add meg mire szeretnéd váltani (cm, m, km):")
+        atvaltas = input()
+
+        if mertekegyseg == "cm":
+
+                if atvaltas == "cm":
+                    print(szam, mertekegyseg, "-", szam, atvaltas)
+                elif atvaltas == "m":
+                    M = szam/100
+                    print(szam, mertekegyseg, "-", M, atvaltas)
+                elif atvaltas == "km":
+                    KM = szam/100000
+                    print(szam, mertekegyseg, "-", KM, atvaltas)
+
+        if mertekegyseg == "m":
+
+                if atvaltas == "cm":
+                    CM = szam*100
+                    print(szam, mertekegyseg, "-", CM, atvaltas)
+                elif atvaltas == "m":
+                    print(szam, mertekegyseg, "-", szam, atvaltas)
+                elif atvaltas == "km":
+                    KM = szam/1000
+                    print(szam, mertekegyseg, "-", KM, atvaltas)
+
+        if mertekegyseg == "km":
+
+                if atvaltas == "cm":
+                    CM = szam*100000
+                    print(szam, mertekegyseg, "-", CM, atvaltas)
+                elif atvaltas == "m":
+                    M = szam*1000
+                    print(szam, mertekegyseg, "-", M, atvaltas)
+                elif atvaltas == "km":
+                    print(szam, mertekegyseg, "-", szam, atvaltas)
