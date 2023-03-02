b2289cdb3c17f0a4e757a60900d17230a882e222
AramiszGago <114133710+AramiszGago@users.noreply.github.com>
2023-03-02 10:22:44 +0100

    Add files via upload

--- ÓraiGyak_23_03_02_2.py
+++ ÓraiGyak_23_03_02_2.py
@@ -1,17 +1,21 @@
 konyvek = {}
 while True:
-    cimek = []
-    szerzo = input()
+    cimek = {}
+    szerzo = input("Addj meg egy szerzőt:")
 
     if szerzo == "":
         break
     else:
         while True:
-            cim = input()
+            cim = input("Addj meg egy könyv címét:")
             if cim == "":
                 break
             else:
-                cimek.append(cim)
+                kiadas = int(input("Add meg a könyv kiadási dátumát:"))
+                mufaj = input("Add meg a műfaját:")
+                leiras = input("Röviden ird le a mű tartalmát:")
+
+                cimek[cim] = {"Kiadási év":kiadas, "Műfaj":mufaj, "Tartalma":leiras}
             konyvek[szerzo] = cimek
 
 for i,j in konyvek.items():
