fd3b8d846367692f0dd493f995016250313e8c6e
AramiszGago <114133710+AramiszGago@users.noreply.github.com>
2023-03-09 08:34:22 +0100

    Add files via upload

--- ÓraiGyak_23_03_02_2.py
+++ ÓraiGyak_23_03_02_2.py
@@ -15,11 +15,18 @@ while True:
                 mufaj = input("Add meg a műfaját:")
                 leiras = input("Röviden ird le a mű tartalmát:")
 
-                cimek[cim] = {"Kiadási év":kiadas, "Műfaj":mufaj, "Tartalma":leiras}
+                cimek[cim] = {"Kiadási év:":kiadas, "Műfaj:":mufaj, "Tartalma:":leiras}
             konyvek[szerzo] = cimek
 
-for i,j in konyvek.items():
-    print(i," : ",j)
+for szerzo,cimek in konyvek.items():
+    print("Szerző:",szerzo,)
+    for cim,adatok in cimek.items():
+        print("\t könyv:", cim,)
+        for Adat_kulcs, Adat_érték in adatok.items():
+            print("\t\t", Adat_kulcs, Adat_érték)
+
+#for i,j in konyvek.items():
+    #print(i," : ",j)
 
 #for i in konyvek.keys():
     #print(i," : ",konyvek[i])
